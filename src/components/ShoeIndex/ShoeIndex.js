7491464464c3d89a72624871b751f5f43056e69a
John <john.19col@gmail.com>
2024-01-09 07:25:16 -0800

    Update ShoeIndex to be responsive

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,43 +1,37 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { WEIGHTS } from '../../constants';
+import { WEIGHTS } from "../../constants";
 
-import Breadcrumbs from '../Breadcrumbs';
-import Select from '../Select';
-import Spacer from '../Spacer';
-import ShoeSidebar from '../ShoeSidebar';
-import ShoeGrid from '../ShoeGrid';
+import { CurrentBreadcrumbs } from "../Breadcrumbs/CurrentBreadcrumbs";
+import ShoeGrid from "../ShoeGrid";
+import ShoeSidebar from "../ShoeSidebar";
+import Spacer from "../Spacer";
+import { SortControl } from "./SortControl";
 
-const ShoeIndex = ({ sortId, setSortId }) => {
+const ShoeIndex = (props) => {
+  const activeCategory = {
+    route: "running",
+    label: "Running",
+  };
   return (
     <Wrapper>
       <MainColumn>
         <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
+          <HeaderLeftSection>
+            <BreadcrumbsHiddenOnDesktop />
+            <Title>{activeCategory.label}</Title>
+          </HeaderLeftSection>
+          <SortControlHiddenOnMobile {...props} />
         </Header>
         <Spacer size={34} />
         <ShoeGrid />
       </MainColumn>
-      <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
+      <LeftColumnVisibleOnDesktop>
+        <CurrentBreadcrumbs />
         <Spacer size={42} />
-        <ShoeSidebar />
-      </LeftColumn>
+        <ShoeSidebar activeCategory={activeCategory.route} />
+      </LeftColumnVisibleOnDesktop>
     </Wrapper>
   );
 };
@@ -48,8 +42,12 @@ const Wrapper = styled.div`
   gap: 32px;
 `;
 
-const LeftColumn = styled.div`
+const LeftColumnVisibleOnDesktop = styled.div`
   flex-basis: 248px;
+
+  @media ${(props) => props.theme.queries.tabletAndBelow} {
+    display: none;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -60,11 +58,28 @@ const Header = styled.header`
   display: flex;
 `;
 
+const HeaderLeftSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: auto;
+`;
+
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
+`;
 
-  margin-right: auto;
+const BreadcrumbsHiddenOnDesktop = styled(CurrentBreadcrumbs)`
+  display: none;
+  @media ${(props) => props.theme.queries.tabletAndBelow} {
+    display: revert;
+  }
+`;
+
+const SortControlHiddenOnMobile = styled(SortControl)`
+  @media ${(props) => props.theme.queries.mobile} {
+    display: none;
+  }
 `;
 
 export default ShoeIndex;
