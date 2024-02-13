15be6c4c01d07147f8cc752214ce3643c327e712
John <john.19col@gmail.com>
2024-02-13 06:19:15 -0800

    Add additional media queries for motion preference and screen resolution

--- src/mediaQueries.js
+++ src/mediaQueries.js
@@ -6,13 +6,31 @@ const breakpoints = {
 const defaultFontSize = 16;
 const toRems = (pixels) => `${pixels / defaultFontSize}rem`;
 
-export const mediaQueries = {
-  mobile: `(max-width: ${toRems(breakpoints.mobile)})`,
-  tablet: `(min-width: ${toRems(
-    breakpoints.mobile + 1
-  )}) and (max-width: ${toRems(breakpoints.tablet)})`,
-  desktop: `(min-width: ${toRems(breakpoints.tablet + 1)})`,
+const resolutionMediaQueries = {
+  mobile:         `(max-width: ${toRems(breakpoints.mobile)})`,
+  tablet:         `(min-width: ${toRems(breakpoints.mobile + 1)}) and (max-width: ${toRems(breakpoints.tablet)})`,
+  desktop:        `(min-width: ${toRems(breakpoints.tablet + 1)})`,
   tabletAndBelow: `(max-width: ${toRems(breakpoints.tablet)})`,
+};
+
+const motionPreferenceMediaQueries = {
   reducedMotionPreference: `(prefers-reduced-motion: reduce)`,
-  noMotionPreference: `(prefers-reduced-motion: no-preference)`,
+  noMotionPreference:      `(prefers-reduced-motion: no-preference)`,
+};
+
+const resolutionAndMotionPreferenceMediaQueries = {
+  mobileAndReducedMotionPreference:         `${resolutionMediaQueries.mobile}         and ${motionPreferenceMediaQueries.reducedMotionPreference}`,
+  mobileAndNoMotionPreference:              `${resolutionMediaQueries.mobile}         and ${motionPreferenceMediaQueries.noMotionPreference}`,
+  tabletAndReducedMotionPreference:         `${resolutionMediaQueries.tablet}         and ${motionPreferenceMediaQueries.reducedMotionPreference}`,
+  tabletAndNoMotionPreference:              `${resolutionMediaQueries.tablet}         and ${motionPreferenceMediaQueries.noMotionPreference}`,
+  desktopAndReducedMotionPreference:        `${resolutionMediaQueries.desktop}        and ${motionPreferenceMediaQueries.reducedMotionPreference}`,
+  desktopAndNoMotionPreference:             `${resolutionMediaQueries.desktop}        and ${motionPreferenceMediaQueries.noMotionPreference}`,
+  tabletAndBelowAndReducedMotionPreference: `${resolutionMediaQueries.tabletAndBelow} and ${motionPreferenceMediaQueries.reducedMotionPreference}`,
+  tabletAndBelowAndNoMotionPreference:      `${resolutionMediaQueries.tabletAndBelow} and ${motionPreferenceMediaQueries.noMotionPreference}`,
+}
+
+export const mediaQueries = {
+  ...resolutionMediaQueries,
+  ...motionPreferenceMediaQueries,
+  ...resolutionAndMotionPreferenceMediaQueries,
 };
